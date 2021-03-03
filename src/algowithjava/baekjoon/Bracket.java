f9b9bc0de4a475395d043c3e85eb61d4074bda13
ohmjeemin <ohmjeemin@gmail.com>
2021-03-03 22:21:35 +0900

    ..

---
+++ src/algowithjava/baekjoon/Bracket.java
@@ -0,0 +1,38 @@
+package algowithjava.baekjoon;
+import java.util.*;
+import java.io.*;
+
+public class Bracket {
+
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            int a = Integer.parseInt(bf.readLine());
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0; i<a; i++) {
+                sb.append(checkVPS(bf.readLine())+"\n");
+            }
+            System.out.println(sb.toString());
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static String checkVPS(String bracket) {
+        int left=0;
+        Stack<Character> stack = new Stack<>();
+
+        for(char c:bracket.toCharArray()) {
+            if(c=='(') {
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()) return "NO";
+                else stack.pop();
+            }
+        }
+        return stack.size()==0?"YES":"NO";
+    }
+}
