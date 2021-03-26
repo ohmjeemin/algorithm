ae87d36bcebd5c49ba5285d813e68c0581a4fe5a
ohmjeemin <ohmjeemin@gmail.com>
2021-03-26 22:19:13 +0900

    균형있는 세상 푸는중

---
+++ src/algowithjava/baekjoon/BalancedWorld.java
@@ -0,0 +1,22 @@
+package algowithjava.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BalancedWorld {
+
+    public void main(String[] args) throws IOException {
+      //  Scanner sc = new Scanner(System.in);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while(true) {
+            String str = bf.readLine();
+            if(".".equals(str)) return;
+        }
+
+    }
+
+}
