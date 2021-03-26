32a6d20e8ed00ae3ee489b168250b0fa1142fa3f
ohmjeemin <ohmjeemin@gmail.com>
2021-03-27 00:46:26 +0900

    ..

--- src/algowithjava/baekjoon/BalancedWorld.java
+++ src/algowithjava/baekjoon/BalancedWorld.java
@@ -7,16 +7,45 @@ import java.util.*;
 
 public class BalancedWorld {
 
-    public void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
       //  Scanner sc = new Scanner(System.in);
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder sb = new StringBuilder();
 
-        while(true) {
-            String str = bf.readLine();
-            if(".".equals(str)) return;
+            while (true) {
+                String s = br.readLine();
+                if (".".equals(s)) {
+                    break ;
+                }
+                sb.append(check(s)+"\n");
+
+            }
+            System.out.println(sb.toString());
+            br.close();
+        }catch(Exception e) {
+            System.out.println("오류우");
         }
+    }
 
+
+    public static String check(String str) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: str.toCharArray()) {
+
+            switch(c) {
+                case '(':
+                case '[':  stack.push(c); break;
+                case ')': if(!stack.isEmpty() && stack.peek()=='(') stack.pop();
+                          else return "no";
+                break;
+                case ']': if(!stack.isEmpty() && stack.peek()=='[') stack.pop();
+                          else return "no";
+                break;
+            }
+        }
+        return stack.isEmpty() ? "yes" : "no";
     }
 
 }
