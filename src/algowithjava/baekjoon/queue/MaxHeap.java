c8a2702b59d2caa3ff7a158d8c1b44aa2f3ab51b
ohmjeemin <ohmjeemin@gmail.com>
2021-04-29 00:21:04 +0900

    ..

--- src/algowithjava/baekjoon/queue/MaxHeap.java
+++ src/algowithjava/baekjoon/queue/MaxHeap.java
@@ -7,33 +7,28 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class maxHeap {
+public class MaxHeap {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         List<Integer> list = new ArrayList<>();
 
         int n = Integer.parseInt(br.readLine()); // n개의 수열을 만들겠다
-        List<Integer> newList = new ArrayList<>();
+
         while(n-->0){
             int input = Integer.parseInt(br.readLine());
             if(input==0) {
                 if(!list.isEmpty()) {
-                    newList.add(Collections.max(list));
+                    sb.append(Collections.max(list)).append("\n");
                     list.remove(Collections.max(list));
                 }else {
-                    newList.add(0);
+                    sb.append(0).append("\n");
                 }
             }else {
                 list.add(input);
             }
         }
-
-        for(int num: newList){
-            System.out.println(num);
-        }
-
+        System.out.println(sb);
     }
-
 
 }
