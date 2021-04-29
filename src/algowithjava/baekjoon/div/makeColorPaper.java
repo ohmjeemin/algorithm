1b7bb3d932177a58c2d45e4229e57873dc1e30b4
ohmjeemin <ohmjeemin@gmail.com>
2021-04-30 01:30:06 +0900

    2630 색종이 만들기

--- src/algowithjava/baekjoon/div/makeColorPaper.java
+++ src/algowithjava/baekjoon/div/makeColorPaper.java
@@ -1,4 +1,27 @@
 package algowithjava.baekjoon.div;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class makeColorPaper {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        int[][] paper = new int[cnt][cnt];
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<cnt; i++) {
+            sb.append(br.readLine());
+        }
+
+        for(int i=0; i<cnt; i++) {
+            for(int j=0; j<cnt; j++) {
+                System.out.println(paper[i][j]+" ");
+            }
+        }
+
+
+
+    }
 }
