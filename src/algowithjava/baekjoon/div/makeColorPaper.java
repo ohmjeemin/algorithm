4b4a35af8baf70f5e36e34f7f76085643670ecc3
ohmjeemin <ohmjeemin@gmail.com>
2021-04-30 21:48:44 +0900

    백준 - 분할 정복 - 색종이 만들기

--- src/algowithjava/baekjoon/div/makeColorPaper.java
+++ src/algowithjava/baekjoon/div/makeColorPaper.java
@@ -1,27 +1,70 @@
 package algowithjava.baekjoon.div;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class makeColorPaper {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int cnt = Integer.parseInt(br.readLine());
-        int[][] paper = new int[cnt][cnt];
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<cnt; i++) {
-            sb.append(br.readLine());
-        }
+    public static int white = 0;
+    public static int blue = 0;
+    public static int[][] board;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int cnt = scanner.nextInt();
+
+        board = new int[cnt][cnt];
 
         for(int i=0; i<cnt; i++) {
             for(int j=0; j<cnt; j++) {
-                System.out.println(paper[i][j]+" ");
+                board[i][j] = scanner.nextInt();
             }
         }
 
+        partition(0,0, cnt);
+
+        System.out.println(white);
+        System.out.println(blue);
+
+    }
+
+    public static void partition(int row, int col, int size) {
+
+        if(colorCheck(row, col, size)) {
+            if(board[row][col] == 0) {
+                white++;
+            }
+            else {
+                blue++;
+            }
+            return;
+        }
+
+        int newSize = size / 2;
+        // 재귀 호출
+        partition(row, col, newSize); // 2사분면
+        partition(row, col + newSize, newSize); // 1사분면
+        partition(row + newSize, col, newSize); // 3사분면
+        partition(row + newSize, col + newSize, newSize); // 4사분면
 
 
+    }
+
+    // 현재 파티션의 컬러가 같은지 체크
+    public static boolean colorCheck(int row, int col, int size) {
+
+        int color = board[row][col]; // 첫 번째 원소를 기준으로 검사
+
+        for(int i = row; i < row+size; i++) {
+            for(int j = col; j < col+size; j++) {
+
+                if(board[i][j] != color) {  // 색상이 같지 않다면 false를 리턴
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
