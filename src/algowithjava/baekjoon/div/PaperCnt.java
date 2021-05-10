39226f28cc515f8677dc29c98870b99be0661899
ohmjeemin <ohmjeemin@gmail.com>
2021-05-10 23:29:44 +0900

    백준:분할정복 - 종이의 갯수

--- src/algowithjava/baekjoon/div/PaperCnt.java
+++ src/algowithjava/baekjoon/div/PaperCnt.java
@@ -1,8 +1,6 @@
 package algowithjava.baekjoon.div;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class PaperCnt  {
     public static int[][] board;
