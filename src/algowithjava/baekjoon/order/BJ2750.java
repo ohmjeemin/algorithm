9af526681d3f43d17e72640c352cfdf3d35b954d
ohmjeemin <ohmjeemin@gmail.com>
2021-06-21 23:51:06 +0900

    정렬 3종 세트

--- src/algowithjava/baekjoon/order/BJ2750.java
+++ src/algowithjava/baekjoon/order/BJ2750.java
@@ -7,19 +7,29 @@ import java.util.*;
 * 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
 * */
 public class BJ2750 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int cnt = Integer.parseInt(br.readLine());
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for(int i=0; i<cnt; i++) {
-           numbers.add(Integer.parseInt(br.readLine()));
-        }
-        Collections.sort(numbers);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int[] numbers = new int[cnt];
+        int temp;
 
-        for(int num : numbers) {
-            sb.append(num+"\n");
+        for(int i=0; i<cnt; i++) {
+            numbers[i] = sc.nextInt();
         }
-        System.out.println(sb);
+
+        for(int i=0; i<cnt-1; i++) {
+            for(int j=0; j<cnt-1; j++) {
+                if(numbers[j]>numbers[j+1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+
+        for(int i=0; i<cnt; i++){
+            System.out.println(numbers[i]);
+        }
+
     }
 }
--- src/algowithjava/baekjoon/order/BJ2751.java
+++ src/algowithjava/baekjoon/order/BJ2751.java
@@ -1,11 +1,25 @@
 package algowithjava.baekjoon.order;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class BJ2751 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int cnt = sc.nextInt();
-        int[] numbers = new int[cnt];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int cnt = Integer.parseInt(br.readLine());
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i=0; i<cnt; i++) {
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(numbers);
+
+        for(int num : numbers) {
+            sb.append(num+"\n");
+        }
+        System.out.println(sb);
 
     }
 }
