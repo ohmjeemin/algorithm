192187add07dcb1e4073d841b9f6ac109ed57e33
ohmjeemin <ohmjeemin@gmail.com>
2021-06-26 23:57:24 +0900

    백준 1427번: 소트인사이드

---
+++ src/algowithjava/baekjoon/order/BJ37006.java
@@ -0,0 +1,16 @@
+package algowithjava.baekjoon.order;
+
+/**
+ * 소트인사이드
+ * 배열을 정렬하는 것은 쉽다. 수가 주어지면 그 수의 각 자리수를 내림차순으로 정렬하자.
+ */
+import java.util.*;
+public class BJ37006 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+//        for(int i=0; i<N.)
+    }
+}
