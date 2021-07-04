31b49eb70fcca0809f8273a741d8c88658996788
ohmjeemin <ohmjeemin@gmail.com>
2021-07-04 18:11:58 +0900

    백준 정렬 (Comparator 직접 구현하기)

---
+++ src/algowithjava/baekjoon/order/BJ10814.java
@@ -0,0 +1,39 @@
+package algowithjava.baekjoon.order;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class BJ10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        String[][] arr = new String[N][2];
+
+        StringTokenizer st;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = st.nextToken(); // 나이
+            arr[i][1] = st.nextToken(); // 이름
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                // 나이순으로 정렬
+               return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]) ;
+            }
+        });
+
+        for(int i=0; i<N; i++) {
+            sb.append(arr[i][0]+" "+arr[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+}
