23bb3dad1a25f81df7943f761b707bbdcc530f32
ohmjeemin <ohmjeemin@gmail.com>
2021-07-09 23:03:21 +0900

    백준 백트래킹

---
+++ src/algowithjava/baekjoon/backtracking/BJ15649.java
@@ -0,0 +1,27 @@
+package algowithjava.baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ15649 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+
+
+        while(N-->0) { // 최대값
+                         // 수열의 길이
+
+
+            sb.append("\n");
+        }
+
+
+        System.out.println(sb);
+    }
+}
