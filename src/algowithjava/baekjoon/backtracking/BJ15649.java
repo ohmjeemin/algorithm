976840531dc20c07d2e0913fcd47fbb344f29280
ohmjeemin <ohmjeemin@gmail.com>
2021-07-11 23:46:10 +0900

    백준 n과m

--- src/algowithjava/baekjoon/backtracking/BJ15649.java
+++ src/algowithjava/baekjoon/backtracking/BJ15649.java
@@ -6,22 +6,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ15649 {
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        StringBuilder sb = new StringBuilder();
 
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+        System.out.println(sb);
 
-        while(N-->0) { // 최대값
-                         // 수열의 길이
+    }
 
-
-            sb.append("\n");
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
         }
 
-
-        System.out.println(sb);
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
     }
 }
