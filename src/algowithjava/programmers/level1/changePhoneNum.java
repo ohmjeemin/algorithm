d96f5fa399b5c4cc79ece782deaaa3b49b8c82d1
ohmjeemin <ohmjeemin@gmail.com>
2021-09-11 23:50:50 +0900

    전화번호바꾸기

---
+++ src/algowithjava/programmers/level1/changePhoneNum.java
@@ -0,0 +1,12 @@
+package algowithjava.programmers.level1;
+
+public class changePhoneNum {
+    public static void main(String[] args) {
+        System.out.println(solution("01033334444"));
+    }
+    public static String solution(String phone_number) {
+        String answer = "";
+        char[] arr = phone_number.toCharArray();
+        return answer;
+    }
+}
