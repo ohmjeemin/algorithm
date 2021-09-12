00e8ff1f23b67594ef88a7ce8fbcfe085492fdc2
ohmjeemin <ohmjeemin@gmail.com>
2021-09-12 21:18:08 +0900

    프로그래머스lv1 - 핸드폰 가리기

--- src/algowithjava/programmers/level1/changePhoneNum.java
+++ src/algowithjava/programmers/level1/changePhoneNum.java
@@ -6,7 +6,10 @@ public class changePhoneNum {
     }
     public static String solution(String phone_number) {
         String answer = "";
-        char[] arr = phone_number.toCharArray();
+        for(int i=0; i<phone_number.length()-4; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length()-4, phone_number.length());
         return answer;
     }
 }
