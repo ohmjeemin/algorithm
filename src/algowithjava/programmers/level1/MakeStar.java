c8fc585f1eb702362931f12ffd6367ae47d3bad1
ohmjeemin <ohmjeemin@gmail.com>
2021-09-18 23:57:37 +0900

    직사각형 별찍기 코틀린!

--- src/algowithjava/programmers/level1/MakeStar.java
+++ src/algowithjava/programmers/level1/MakeStar.java
@@ -17,3 +17,20 @@ public class MakeStar {
 
     }
 }
+/*
+kotlin
+* fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    var star : String = ""
+    for(i in 1..a){
+        star += "*"
+    }
+
+    for(i in 1..b){
+        println(star)
+    }
+
+}
+
+*
+* */
