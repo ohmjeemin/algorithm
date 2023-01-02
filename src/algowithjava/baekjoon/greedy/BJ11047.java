7ec43d2320d30a16eecdeb47acce6a3fe4432daa
ohmjeemin <ohmjeemin@mobility42.io>
2023-01-02 23:27:57 +0900

    test

--- src/algowithjava/baekjoon/greedy/BJ11047.java
+++ src/algowithjava/baekjoon/greedy/BJ11047.java
@@ -18,7 +18,7 @@ public class BJ11047 {
         for(int i=0; i<n; i++) {
             kind[i] = Integer.parseInt(br.readLine());
         }
-
+        //test
         int cnt = 0;
         for(int i=n-1; i>=0; i--) {
             if(kind[i] <= k) {
