7714ad01449582cc117e8ec576b3f018277ce683
ohmjeemin <ohmjeemin@gmail.com>
2023-02-09 23:23:38 +0900

    test

--- src/algowithjava/baekjoon/loop/BJ10950.java
+++ src/algowithjava/baekjoon/loop/BJ10950.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class BJ10950 {
+    //test
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
