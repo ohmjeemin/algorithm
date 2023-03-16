09e25416d70655302375828b15aecc1745278348
ohmjeemin <ohmjeemin@gmail.com>
2023-03-16 23:56:34 +0900

    BJ2908

--- src/algowithjava/baekjoon/string/BJ2908.java
+++ src/algowithjava/baekjoon/string/BJ2908.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BJ1152 {
+public class BJ2908 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine(), " ");
-        System.out.println(st.countTokens());
+        String str1 = st.nextToken();
+        String str2 = st.nextToken();
+        char[] charArr1 = str1.toCharArray();
+
+        int b = Integer.parseInt(st.nextToken());
+
     }
 }
