0a55f730b5a28a2039a9bb30b931ec222e02d189
ohmjeemin <ohmjeemin@gmail.com>
2023-03-22 23:57:52 +0900

    BJ11718

---
+++ src/algowithjava/baekjoon/string/BJ11718.java
@@ -0,0 +1,17 @@
+package algowithjava.baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while ((str = br.readLine()) != null) {
+            sb.append(str+"\n");
+        }
+        System.out.println(sb);
+    }
+}
