c9ab0c737edb3e09e517fd030460c1a55643a2bf
ohmjeemin <ohmjeemin@gmail.com>
2023-03-26 22:57:09 +0900

    BJ25314

---
+++ src/algowithjava/baekjoon/loop/BJ25314.java
@@ -0,0 +1,21 @@
+package algowithjava.baekjoon.loop;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ25314 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n/4; i++) {
+            sb.append("long ");
+        }
+        sb.append("int");
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception {
+        new BJ25314().solution();
+    }
+}
+}
