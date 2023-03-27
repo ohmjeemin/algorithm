fdad5c9bc6930aa579345cf7bb8a5eb3cd1a9708
ohmjeemin <ohmjeemin@gmail.com>
2023-03-27 23:52:59 +0900

    BJ10171

---
+++ src/algowithjava/baekjoon/BJ10171.java
@@ -0,0 +1,13 @@
+package algowithjava.baekjoon;
+
+public class BJ10171 {
+    public static void main (String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.print("||_/=\\");
+        System.out.print("\\");
+        System.out.print("__|");
+    }
+}
