88a38119f2c91b4fba0a6128d2da932a3cfdb782
ohmjeemin <ohmjeemin@gmail.com>
2023-04-03 23:53:40 +0900

    BJ2751

---
+++ src/algowithjava/baekjoon/order/BJ2751_2.java
@@ -0,0 +1,21 @@
+package algowithjava.baekjoon.order;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ2751_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int loopCnt = Integer.parseInt(br.readLine());
+        int[] arr = new int[loopCnt];
+        for(int i=0; i<loopCnt; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int i=loopCnt; i>0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
